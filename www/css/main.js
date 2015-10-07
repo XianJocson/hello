b058d6a75840ab968e8a6a96f7161308450eb983
abenham96 <a.benham96@gmail.com>
2015-10-07 13:19:16 +1100

    V.1.2.10

--- www/css/main.js
+++ www/css/main.js
@@ -4,10 +4,10 @@ window.onload = function () {
     var slideMenuButton = document.getElementById('slide-menu-button');
     slideMenuButton.onclick = function (e) {
         var cl = document.body.classList;
-        if (cl.contains('left-nav')) {
-            cl.remove('left-nav');
+        if (cl.contains('side-nav')) {
+            cl.remove('side-nav');
         } else {
-            cl.add('left-nav');
+            cl.add('side-nav');
         }
     };
  
